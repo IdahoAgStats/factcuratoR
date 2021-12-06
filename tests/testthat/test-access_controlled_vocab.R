ce01bd5aac17006854098019a59bfcf8836fd035
justjacqueline <jacquelinejtay@gmail.com>
2021-12-06 14:05:09 +0900

    Edit tests to run within package

--- tests/testthat/test-access_controlled_vocab.R
+++ tests/testthat/test-access_controlled_vocab.R
@@ -1,18 +1,25 @@
-test_that("get.variety_db returns the expected variety names", {
-  test <- get.variety_db(select_before = "2021-01-01")
-  test2 <- test %>% select(date_added) %>% unique(.)
+controlled_vocab_folder <- testthat::test_path("test_controlled_vocab")
+library(magrittr)
+
+test_that("get.variety_db() returns the expected variety names", {
+  test <- get.variety_db(controlled_vocab_folder, select_before = "2021-01-01")
+  test2 <- test %>% dplyr::select(date_added) %>% unique(.)
 
   expect_equal(nrow(test2), 1)
 })
 
-test_that("get.variety_db returns the expect crops", {
-  test <- get.variety_db(select_crops = "wheat")
-  test2 <- test %>% select(crop_db) %>% unique(.) %>%
-    mutate(test = str_detect(crop_db, "Wheat"))
+test_that("get.variety_db() returns the expect crops", {
+  test <- get.variety_db(controlled_vocab_folder, select_crops = "wheat")
+  test2 <- test %>%
+    dplyr::select(crop_db) %>%
+    unique(.) %>%
+    dplyr::mutate(test = stringr::str_detect(crop_db, "Wheat"))
 
   expect_equal(all(test2$test), TRUE)
 })
 
 test_that("list.db_var() correctly throws error", {
-  expect_error(list.db_var("loc"), "'codebook_name' doesn't match a codebook name")
+  expect_error(list.db_var(controlled_vocab_folder, "loc"),
+               "'codebook_name' doesn't match a codebook name")
 })
+
