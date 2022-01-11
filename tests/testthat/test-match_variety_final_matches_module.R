332c3b96b29ebaa39e26e0da207c9b1f6223ca49
justjacqueline <jacquelinejtay@gmail.com>
2022-01-11 13:28:33 +0900

    Add @export and test for collect_final_matches()

---
+++ tests/testthat/test-match_variety_final_matches_module.R
@@ -0,0 +1,29 @@
+outputfolder_test <- testthat::test_path("test_match_variety_files",
+                                         "output")
+
+test_that("collect_final_matches() returns correct output", {
+  # Create test data in the form that collect_final_matches() expects
+  names <-
+    data.frame(
+      variety = c("name1", "name2", "name3", "name4"),
+      var_id = 1:4,
+      type = NA,
+      crop_type = NA) %>%
+    mutate(intid = variety)
+
+  df1 <- names %>% filter(var_id <= 2) %>%
+    mutate(variety_db = variety, intid_db = variety, type_db = NA)
+  df2 <- names %>% filter(var_id == 3) %>%
+    mutate(variety_db = variety, intid_db = variety, type_db = NA)
+
+  df_blank <- data.frame(a = NA, b = NA)
+
+  ls1 <- list(match = df1, nomatch = df_blank)
+  ls2 <- list(match = df2, nomatch = df_blank)
+
+  test <-
+    collect_final_matches(list(exact = ls1,
+                               rename = ls2), names, outputfolder_test)
+
+  expect_equal(test$match_step, c("exact", "exact", "rename", NA))
+})
