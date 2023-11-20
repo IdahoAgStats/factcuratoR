b198b52703b1b3f9377df1a3e33fc0d84b791f47
emilymg <emilymgalvin@gmail.com>
2023-11-20 14:24:23 -0500

    add package-level imports

---
+++ R/factcuratoR-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats na.omit
+#' @importFrom stats setNames
+#' @importFrom utils capture.output
+#' @importFrom utils read.csv
+#' @importFrom utils write.csv
+## usethis namespace: end
+NULL
